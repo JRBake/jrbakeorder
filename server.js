11928b1f3409ce95fd245684d80b165b927378ee
JRBake <116965485+JRBake@users.noreply.github.com>
2026-05-09 14:15:40 -0400

    Changed port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // --- GOOGLE API SETUP ---
 const oauth2Client = new google.auth.OAuth2(
